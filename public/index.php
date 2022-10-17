4950975f466765568acce242ea8d4834c3414c98
Cesar Acual <agustoacual@gmail.com>
2022-10-17 16:59:27 -0600

    Refactor EntryPoint create loadTemplate function

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+function loadTemplate($template, $variables = [])
+{
+	extract($variables);
+
+	ob_start();
+
+	include __DIR__ . "/../templates/{$template}";
+
+	return ob_get_clean();
+}
+
 try {
 	include __DIR__ . '/../includes/DatabaseConnection.php';
 	include __DIR__ . '/../classes/DatabaseTable.php';
@@ -14,16 +25,8 @@ try {
 	$page = $recipeController->$action();
 
 	$title = $page['title'];
-	
-	if (isset($page['variables'])) {
-		extract($page['variables']);
-	}
-
-	ob_start();
-
-	include __DIR__ . "/../templates/{$page['template']}";
-
-	$output = ob_get_clean();
+	$variables = $page['variables'] ?? [];
+	$output = loadTemplate($page['template'], $variables);
 } catch (PDOException $e) {
 	$title = "An error has ocurred";
 	$output = sprintf("Database error: %s in %s:%s",
