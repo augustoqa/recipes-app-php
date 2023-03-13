1c8cf6b01c006905fafd6140b462307a1a9e3577
Augusto Acual <augustoacual@gmail.com>
2023-03-13 13:17:32 -0600

    Using composer for autoload

--- includes/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function autoloader($className) {
-	$fileName = str_replace('\\', '/', $className);
-	$file = __DIR__ . "/../{$fileName}.php";
-	include $file;
-}
-
-spl_autoload_register('autoloader');
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../includes/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $uri = strtok(ltrim($_SERVER['REQUEST_URI'], '/'), '?');
 	
